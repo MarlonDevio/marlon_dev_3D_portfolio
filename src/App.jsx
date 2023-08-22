8be88d017c390cc67098f5e930adb250942b391a
MarlonDevio <mar.hamaekers@gmail.com>
2023-08-22 19:04:27 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+} from "./components";
+import { StarsCanvas } from "./components/canvas/index.js";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className=" bg-cover bg-no-repeat bg-center hero-wrapper">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+import { styles } from "../styles.js";
+import { ComputersCanvas } from "./index.js";
+
+const Hero = () => {
+  return <section className="relative w-full h-screen mx-auto"></section>;
+};
+
+export default Hero;
